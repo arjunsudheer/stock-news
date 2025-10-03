6d82dc137464e92bb083fd51127a923e90079505
Arjun Sudheer <arjunsudheer05@outlook.com>
2025-10-02 23:40:40 -0700

    Update script to fix logging errors
    Update autoemail to handle missing keywords in summary

--- send_stock_analysis_email.sh
+++ send_stock_analysis_email.sh
@@ -1,9 +1,12 @@
-cd $HOME/code/personal_projects/stock-news
+#!/bin/bash
+
+cd /home/arjunsudheer/code/personal_projects/stock-news/
+
 source venv/bin/activate
 
 # Clear any existing logs or output files
 rm -f output.log
-rm -f analysis_report.txt
+rm -f error.log
 
 # Run the script
 python3 main.py
